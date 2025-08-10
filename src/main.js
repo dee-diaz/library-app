454cd66a2ca167ec467dd00a2957429cddc082b4
dee-diaz <diazthediablo@yahoo.com>
2025-08-09 21:15:11 -0300

    Start making modal

--- src/main.js
+++ src/main.js
@@ -1,3 +1,17 @@
-import './app.css'
+import "./app.css";
 // import { setupCounter } from './counter.js'
 
+const addButtons = document.querySelectorAll(".btn-add");
+const dialog = document.querySelector("dialog");
+const closeButtons = document.querySelectorAll("[data-btn-close]");
+
+addButtons.forEach((button) => button.addEventListener("click", showModal));
+closeButtons.forEach((button) => button.addEventListener("click", closeModal));
+
+function showModal() {
+  dialog.showModal();
+}
+
+function closeModal() {
+  dialog.close();
+}

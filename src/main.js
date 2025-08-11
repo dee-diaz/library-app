873b0ca6287e29eee809b2bddd98b6155fe06ec0
dee-diaz <diazthediablo@yahoo.com>
2025-08-11 10:19:34 -0300

    Add book modal UI

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import "./app.css";
 const addButtons = document.querySelectorAll(".btn-add");
 const dialog = document.querySelector("dialog");
 const closeButtons = document.querySelectorAll("[data-btn-close]");
+const selectStatus = document.querySelector("select#status");
+const ratingContainer = document.querySelector("[data-rating]");
+const selectRating = document.querySelector("#rating");
+const addForm = document.querySelector("#add-form");
 
 addButtons.forEach((button) => button.addEventListener("click", showModal));
 closeButtons.forEach((button) => button.addEventListener("click", closeModal));
@@ -15,3 +19,27 @@ function showModal() {
 function closeModal() {
   dialog.close();
 }
+
+selectStatus.addEventListener("change", () => {
+  const value = selectStatus.value;
+  if (value === "read") {
+    ratingContainer.classList.remove("hidden");
+    ratingContainer.classList.add("flex");
+    selectRating.classList.remove("hidden");
+    selectRating.classList.add("flex");
+  } else {
+    ratingContainer.classList.remove("flex");
+    ratingContainer.classList.add("hidden");
+    selectRating.classList.remove("flex");
+    selectRating.classList.add("hidden");
+  }
+
+  // Remove later
+  selectRating.addEventListener("change", () => {
+    console.log(selectRating.value);
+  });
+});
+
+addForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
